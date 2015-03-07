568322b998388f52048c8393878284f49192e597
Hjörtur Larsen <91hjortur@gmail.com>
2015-03-07 21:19:01 +0000

    Fyrsta commit

---
+++ js/iterator.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    console.log("Fetching json...");
+
+    $.getJSON("../json/maena_json.json", function(data) {
+
+        console.log("Working with json data...");
+
+        $.each(data, function(key, val) {
+            $(".container").append($.parseHTML(this.title1));
+            $(".container").append($.parseHTML(this.title2));
+            $(".container").append($.parseHTML(this.content));
+            $(".container").append($.parseHTML(this.author));
+        });
+    });
+});
