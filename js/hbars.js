42cdf254957c1b6bdcc73d0217233def07b5a468
Hjörtur Larsen <91hjortur@gmail.com>
2015-03-11 06:52:17 +0000

    Komin beinagrind í HTML, bakgrunnsmyndir komnar

--- js/hbars.js
+++ js/hbars.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
 
     $.get("json/maena.json", function (data, status, xhr) {
 
-        populate_menu();
+    	writeSection(data);
+
+        function writeSection(data) {
+        	var source = $("#section_template").html();
+        	var template = Handlebars.compile(source);
+        	$(".articles-container").append(template(data));
+        }
 
         function populate_menu() { 
             var source = $("#efnisyfirlit_template").html();
